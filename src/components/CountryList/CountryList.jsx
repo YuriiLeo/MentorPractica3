187b824b6d59356883a2f61c448764a576340990
Maxim Kucheriaviy <maxim-k@i.ua>
2022-10-13 21:16:50 +0300

    coutry list

--- src/components/CountryList/CountryList.jsx
+++ src/components/CountryList/CountryList.jsx
@@ -1,5 +1,12 @@
 import { Grid, GridItem } from 'components';
+import { Link } from 'react-router-dom';
 
 export const CountryList = ({ countries }) => {
-  return <h2>CountryList</h2>;
+  return <Grid>
+    {countries.map(({id, flag}) => <GridItem key={id}>
+      <Link to={`country/${id}`} >
+        <img src={flag} alt={id}/>
+      </Link>
+      </GridItem>)}
+  </Grid>
 };
